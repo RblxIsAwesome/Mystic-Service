9bd914ec6eab590d9fa149e4b3db089b6309ff51
Rblx is ass <yhdry852@gmail.com>
2025-08-12 20:03:36 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,12 +22,10 @@ export const metadata: Metadata = {
   title: "Mystic Services",
   description: "MysticGen | #1 Sites We are the best beaming site out there!",
   icons: {
-    icon: "https://media.discordapp.net/attachments/1401727715564064963/1404962977043447981/mystic_gen_pulse_final.gif?ex=689d193c&is=689bc7bc&hm=9feefea11ae165ed12e420a091d1ea41c16c4f7f6ea5a7b012730e3b15ae3bef&=",
-    shortcut:
-      "https://media.discordapp.net/attachments/1401727715564064963/1404962977043447981/mystic_gen_pulse_final.gif?ex=689d193c&is=689bc7bc&hm=9feefea11ae165ed12e420a091d1ea41c16c4f7f6ea5a7b012730e3b15ae3bef&=",
-    apple:
-      "https://media.discordapp.net/attachments/1401727715564064963/1404962977043447981/mystic_gen_pulse_final.gif?ex=689d193c&is=689bc7bc&hm=9feefea11ae165ed12e420a091d1ea41c16c4f7f6ea5a7b012730e3b15ae3bef&=",
-  },
+  icon: "👑",        // <- Change this emoji or use a URL
+  shortcut: "👑",    // <- Change this too
+  apple: "👑",       // <- And this
+},
   generator: "v0.app",
 }
 
